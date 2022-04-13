65a4a0a4ebc3a6388b14ccb6714febbb0affaf7b
Mr.V <dzulajvita@gmail.com>
2022-04-13 12:38:35 +0300

    components: pizzablock; requests to db.json, prop-types

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import { Header }        from './components';
-import { Home, Cart }    from "./pages";
-import { Routes, Route } from "react-router-dom";
+import { Header }              from './components';
+import { Home, Cart }          from "./pages";
+import { Routes, Route }       from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
 
 function App() {
-  return (
-      <div className="App">
-          <div className="wrapper">
-              <Header />
+    const [pizzas, setPizzas] = useState([]);
 
-              <div className="content">
-                  <Routes>
-                      <Route path='/'     element={<Home />} exact />
-                      <Route path='/cart' element={<Cart />} exact />
-                  </Routes>
-              </div>
-          </div>
-      </div>
-  );
+    useEffect(() => {
+        axios.get('http://localhost:3000/db.json').then(({ data }) => {
+            setPizzas(data.pizzas);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <Header />
+
+                <div className="content">
+                    <Routes>
+                        <Route path='/'     element={<Home items={ pizzas } />} exact />
+                        <Route path='/cart' element={<Cart />} exact />
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;

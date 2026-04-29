e0f895262cef857c55d87c668c08a9c9bdb683bf
v-dzhulai <dzulajvita@gmail.com>
2026-04-29 23:59:53 +0300

    added cart page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './scss/app.scss';
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 
 import {Route, Routes} from "react-router-dom";
@@ -17,6 +18,7 @@ function App() {
                     <div className="container">
                         <Routes>
                             <Route path="/" element={<Home/>}/>
+                            <Route path="/cart" element={<Cart/>}/>
                             <Route path="*" element={<NotFound/>}/>
                         </Routes>
                     </div>
